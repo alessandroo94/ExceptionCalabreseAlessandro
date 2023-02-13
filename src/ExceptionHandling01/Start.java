4054dfb8633291024e7632363f2c45484d901e2f
alessandroo94 <121314484+alessandroo94@users.noreply.github.com>
2023-02-13 11:37:22 +0100

    Update Start.java

--- src/ExceptionHandling01/Start.java
+++ src/ExceptionHandling01/Start.java
@@ -11,9 +11,9 @@ public class Start {
     }
 
     private static void checkStore(double score) {
-        if(0 < score && score < 50.00){
+        if(0 < score && score <= 50.00){
             System.out.println("Average score");
-        } else if (50.00 < score && score < 100.00){
+        } else if (50.00 < score && score <= 100.00){
             System.out.println("Very good score");
         } else {
             throw new ArithmeticException("Score is out of scale!");
